da453a876b513f3e3ed5578a7b599cd5ca4939cf
Teva90 <89648026+Teva90@users.noreply.github.com>
2023-02-10 04:25:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+const navItems = document.querySelector('#nav__items');
+const openNavBtn = document.querySelector('#open__nav-btn');
+const closeNavBtn = document.querySelector('#close__nav-btn');
+
+openNavBtn.addEventListener('click',()=>{
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+})
+const closeNav = () => {
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+}
+closeNavBtn.addEventListener('click', closeNav);
+//close nav menu when a menu item is clicked :
+if (window.innerWidth < 1024) { // in 1024px and smaller screen do this action (the opened nav items when clicked on one of it close the nav list) !
+    document.querySelectorAll('#nav__items li a').forEach(navItem => {
+navItem.addEventListener('click', () => {
+closeNav();
+})
+    })
+}
+// change navbar styles on scroll :
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+})
+// Swiper Js Init :
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    // Responsive Breakpoints
+    breakpoints:{
+        600: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+  });
